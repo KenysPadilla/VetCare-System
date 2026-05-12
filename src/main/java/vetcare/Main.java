402a4f60443ed2ed919563d6a9094f1dbaf2017a
Kenys Padilla <kvpadilla@unicesar.edu.co>
2026-05-11 22:03:23 -0500

    chore: configuración inicial Maven, JavaFX y conexión Oracle

---
+++ src/main/java/vetcare/Main.java
@@ -0,0 +1,24 @@
+package vetcare;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("VetCare System - JavaFX funcionando");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 200);
+        stage.setTitle("VetCare System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
