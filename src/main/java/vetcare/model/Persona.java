fd5346c8209f4cf3afeb6dabc4ea05d46780b1ed
Kenys Padilla <kvpadilla@unicesar.edu.co>
2026-05-17 11:40:35 -0500

    feat: agregar clase abstracta Persona

---
+++ src/main/java/vetcare/model/Persona.java
@@ -0,0 +1,65 @@
+package vetcare.model;
+
+public abstract class Persona {
+
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+
+    public Persona(String cedula, String nombre, String apellido, String telefono, String email) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public abstract String getRol();
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido + " (CC: " + cedula + ")";
+    }
+}
